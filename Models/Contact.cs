bc69da411d3ff8faf928ce5b33a7a25bc24aa423
Joshua Chandler <joshua.chadnler331@gmail.com>
2021-02-12 16:14:02 -0500

    First Commit

---
+++ Models/Contact.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Personal Email")]
+        public string PersonalEmail { get; set; }
+
+        [Display(Name = "Work Email")]
+        public string WorkEmail { get; set; }
+
+        [Display(Name = "Mobile Phone")]
+        public string MobilePhone { get; set; }
+
+        [Display(Name = "Work Phone")]
+        public string WorkPhone { get; set; }
+
+        [Display(Name = "Home Phone")]
+        public string HomePhone { get; set; }
+
+        [Display(Name = "Primary Address")]
+        public string Address1 { get; set; }
+
+        [Display(Name = "Secondary Address")]
+        public string Address2 { get; set; }
+
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Display(Name = "Company")]
+        public string Company { get; set; }
+
+        [Display(Name = "Fax Number")]
+        public string FaxNumber { get; set; }
+
+    }
+}
